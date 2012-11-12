afff386d1814ed350c72abea6254e7f472428931
Sravan <sravanreddy001@gmail.com>
2012-11-12 17:25:37 -0500

    JaccardSimilarity -- Stub code

---
+++ web-based-entity-resolution/src/edu/buffalo/cse/di/util/JaccardSimilarity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package edu.buffalo.cse.di.util;
+
+/**
+ * Calculate the metrics related to Jaccard coefficient.
+ * @author sravanku@buffalo.edu
+ */
+public class JaccardSimilarity {
+    
+    
+    /**
+     * Given two string as input return the JaccardCoefficient of both.
+     * Make use of Stemmer for better scores.
+     * @param str1
+     * @param str2
+     * @return <b>double</b> jaccard coefficient of str1 and str2
+     */
+    public static double getJaccardSimilarty(String str1, String str2) {
+        
+        return 0.0;
+    }
+    
+}
